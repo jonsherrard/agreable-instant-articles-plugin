014044ef10344f7fea748800c5a7547cea2f6cce
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-12-12 09:59:11 +0000

    custom instant article role

--- app/panels.php
+++ app/panels.php
@@ -2,11 +2,18 @@
 
 /** @var \Herbert\Framework\Panel $panel */
 
-$panel->add([
-    'type'   => 'panel',
-    'as'     => 'instantArticlesPanel',
-    'title'  => 'Instant Articles',
-    'slug'   => 'instant-articles-index',
-    'icon'   => 'dashicons-facebook',
-    'uses'   => __NAMESPACE__ . '\Controllers\PanelController@index'
-]);
+\add_action('plugins_loaded', function() use ($panel) {
+  $user = wp_get_current_user();
+  $user_roles = $user->roles;
+
+  if (in_array('instant_articles_editor', $user_roles) || in_array('administrator',$user_roles)) {
+    $panel->add([
+        'type'   => 'panel',
+        'as'     => 'instantArticlesPanel',
+        'title'  => 'Instant Articles',
+        'slug'   => 'instant-articles-index',
+        'icon'   => 'dashicons-facebook',
+        'uses'   => __NAMESPACE__ . '\Controllers\PanelController@index'
+    ]);
+  }
+});
